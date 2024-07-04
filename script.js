bcffa57873bb287f3c71fde15793a31336fe3bbe
kouzuuke <156013328+kouzuuke@users.noreply.github.com>
2024-07-04 23:03:00 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+<script>
+            function showTime() {
+            document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+            }
+            showTime();
+            setInterval(function () {
+                showTime();
+            }, 1000);
+        </script>
