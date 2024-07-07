b6138e67bf2860193081a159f8ba6370cea6d1c2
tohoku <135399339+koumekko@users.noreply.github.com>
2024-07-07 20:25:14 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 
-            function showTime() {
-            document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-            }
-            showTime();
-            setInterval(function () {
-                showTime();
-            }, 1000);
-            console.log(`Hello! I am an alert box!!`);
+            //nothing yet
 
