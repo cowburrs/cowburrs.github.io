078cb199a6f9c3703618e829480563dbe1a4b7ef
koumekko <brycetabangcura@gmail.com>
2024-07-10 01:48:40 +1000

    h6 added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-
-            //nothing yet
-
+function showTime() {
+    let x = new Date().getUTCHours() + 10;
+    let y = "AM";
+    if ((x%24 > 12)) {
+    y = "PM";
+    };
+    x = x%12;
+    let a = "";
+    if (x < 10){
+    a ="0"
+    }
+  document.getElementById("time").innerText = a + x.toString() + ":"+ new Date().getMinutes().toString() + "." +new Date().getSeconds().toString() + y;
+}
+showTime();
+setInterval(function () {
+  showTime();
+}, 1000);
