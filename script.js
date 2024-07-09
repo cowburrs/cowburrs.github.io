e4b7da1bad08bf79f9ab0c28ccf17c35c28f854a
koumekko <brycetabangcura@gmail.com>
2024-07-10 02:10:12 +1000

    fixed clock

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 function showTime() {
-    let x = new Date().getUTCHours() + 10;
-    let y = "AM";
-    if ((x%24 > 12)) {
+  let x = new Date().getUTCHours() + 10;
+  let y = "AM";
+  if (x % 24 > 12) {
     y = "PM";
-    };
-    x = x%12;
-    let a = "";
-    if (x < 10){
-    a ="0"
-    }
-  document.getElementById("time").innerText = a + x.toString() + ":"+ new Date().getMinutes().toString() + "." +new Date().getSeconds().toString() + y;
+  }
+  x = x % 12;
+  let a = "";
+  let b = "";
+  let c = "";
+  if (new Date().getUTCMinutes() < 10) {
+    b = "0";
+  }
+  if (x < 10) {
+    a = "0";
+  }
+  if (new Date().getUTCSeconds() < 10) {
+    c = "0";
+  }
+  document.getElementById("time").innerText =
+    a +
+    x.toString() +
+    ":" +
+    b +
+    new Date().getMinutes().toString() +
+    "." +
+    c +
+    new Date().getSeconds().toString() +
+    y;
 }
 showTime();
 setInterval(function () {
