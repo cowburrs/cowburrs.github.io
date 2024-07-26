cd6cf5d060bb8584b3543da4cf0dd05ad06cb45c
koumekko <brycetabangcura@gmail.com>
2024-07-26 19:43:21 +1000

    f
    tf

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Time Script
 function showTime() {
   let x = new Date().getUTCHours() + 10;
   let y = "AM";
@@ -32,3 +33,10 @@ showTime();
 setInterval(function () {
   showTime();
 }, 500);
+
+//Button Script
+document.getElementById("imagebutton").addEventListener("click", myFunction);
+function myFunction() {
+  document.getElementById("imagebutton").innerHTML = "hellow";
+  document.getElementById("flower").style.height = "100";
+}
